738f25528c4b6e1c4387b48e0abbac2f4f34f174
EDWINGAA <159578458+EDWINGAA@users.noreply.github.com>
2024-04-15 12:10:22 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LayoutHome from './pages/home/layout/layoutHome'
+
+
+function App() {
+
+
+  return (
+      <div>
+
+<LayoutHome/>
+      
+      </div>
+  )
+}
+export default App
